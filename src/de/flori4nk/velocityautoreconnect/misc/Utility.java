flori4nk/VelocityAutoReconnect

commit 8205a3c7c835269e00c39f7ce04fcd64ecb04f01

Code cleanup; Simplified ping check option check; Stop throwing exception when pingcheck=off

src/de/flori4nk/velocityautoreconnect/misc/Utility.java
@@ -1,0 +1,35 @@
+package de.flori4nk.velocityautoreconnect.misc;
+
+import java.util.Optional;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+
+import de.flori4nk.velocityautoreconnect.VelocityAutoReconnect;
+import net.kyori.adventure.text.Component;
+
+public class Utility {
+	
+	// Returns whether or not the names of the servers match.
+	public static boolean doServerNamesMatch(RegisteredServer var0, RegisteredServer var1) {
+		return var0.getServerInfo().getName().equals(var1.getServerInfo().getName());
+	}
+
+	// Check whether or not the welcome message should be sent and act accordingly
+	public static void sendWelcomeMessage(Player player) {
+		if(VelocityAutoReconnect.getConfigurationManager().getBooleanProperty("message.enabled")) {
+			player.sendMessage(Component.text(VelocityAutoReconnect.getConfigurationManager().getProperty("message")));
+		}
+	}
+		
+	public static RegisteredServer getServerByName(String serverName) {
+		Optional<RegisteredServer> optionalServer = VelocityAutoReconnect.getProxyServer().getServer(VelocityAutoReconnect.getConfigurationManager().getProperty("limbo-name"));
+			
+		if(optionalServer.isPresent()) {
+			return optionalServer.get();
+		}
+			
+		VelocityAutoReconnect.getLogger().severe(String.format("Server \"%s\" is invalid, VelocityAutoReconnect will not function!", serverName));
+		return null;
+	}
+
+}

src/de/flori4nk/velocityautoreconnect/VelocityAutoReconnect.java
@@ -26,240 +26,121 @@ SOFTWARE.
 */
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Properties;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
-
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
-import com.velocitypowered.api.event.connection.DisconnectEvent;
-import com.velocitypowered.api.event.player.KickedFromServerEvent;
-import com.velocitypowered.api.event.player.KickedFromServerEvent.RedirectPlayer;
-import com.velocitypowered.api.event.player.ServerPostConnectEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
-import com.velocitypowered.api.proxy.ServerConnection;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
+import de.flori4nk.velocityautoreconnect.listeners.ConnectionListener;
+import de.flori4nk.velocityautoreconnect.listeners.KickListener;
+import de.flori4nk.velocityautoreconnect.misc.Utility;
+import de.flori4nk.velocityautoreconnect.storage.ConfigurationManager;
+import de.flori4nk.velocityautoreconnect.storage.PlayerManager;
 
 
 
 @Plugin(id = "velocityautoreconnect", name = "VelocityAutoReconnect", version = "1.2.0", authors = {"Flori4nK"})
 public class VelocityAutoReconnect {
 	
-	private final ProxyServer server;
-	private Logger logger;
-	private RegisteredServer limboServer;
-	private RegisteredServer directConnectServer;
-	private Map<Player, RegisteredServer> playerData;
-	private File configurationFile;
-	private Properties configuration;
-
-	private Pattern kickFilterWhitelist;
-	private Pattern kickFilterBlacklist;
+	private static ProxyServer proxyServer;
+	private static Logger logger;
+	private static RegisteredServer limboServer;
+	private static RegisteredServer directConnectServer;
+	
+	private static ConfigurationManager configurationManager;
+	private static PlayerManager playerManager;
 	
 	@Inject
 	public VelocityAutoReconnect(ProxyServer server, Logger logger, @DataDirectory Path dataDirectory) {
-		this.server = server;
-		this.logger = logger;
-		this.playerData = new HashMap<Player, RegisteredServer>();
-		this.configurationFile = new File(dataDirectory.toFile(), "velocityautoreconnect.conf");
-		this.configuration = new Properties();
-		try {
-			// Create the configuration file if it doesn't exist.
-			if(!this.configurationFile.exists()) {
-				this.configurationFile.getParentFile().mkdirs();
-				this.configurationFile.createNewFile();
-			}
-			
-			// Set default values
-			configuration.setProperty("limbo-name", "limbo");
-			configuration.setProperty("directconnect-server", "lobby");
-			configuration.setProperty("task-interval-ms", "3500");
-			configuration.setProperty("kick-filter.blacklist", ".* ([Bb]anned|[Kk]icked).*");
-			configuration.setProperty("kick-filter.blacklist.enabled", "false");
-			configuration.setProperty("kick-filter.whitelist", "Server closed");
-			configuration.setProperty("kick-filter.whitelist.enabled", "true");
-			configuration.setProperty("message", "You will be reconnected soon.");
-			configuration.setProperty("message.enabled", "false");
-			configuration.setProperty("pingcheck", "true");
-			
-			// Load saved values
-			configuration.load(new FileInputStream(configurationFile));
-			
-			// Save old values combined with possible new defaults
-			configuration.store(new FileOutputStream(configurationFile), "VelocityAutoReconnect configuration -- Documentation: https://github.com/flori4nk/VelocityAutoReconnect/blob/master/README.md#configuration");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		// Compile whitelist / blacklist Patterns from configured expressions
-		this.kickFilterBlacklist = Pattern.compile(configuration.getProperty("kick-filter.blacklist"), Pattern.DOTALL);
-		this.kickFilterWhitelist = Pattern.compile(configuration.getProperty("kick-filter.whitelist"), Pattern.DOTALL);
+		VelocityAutoReconnect.proxyServer = server;
+		VelocityAutoReconnect.logger = logger;
+		VelocityAutoReconnect.configurationManager = new ConfigurationManager(new File(dataDirectory.toFile(), "velocityautoreconnect.conf"));
+		VelocityAutoReconnect.playerManager = new PlayerManager();
 	}
 	
 	@Subscribe(order = PostOrder.NORMAL)
 	public void onInitialize(ProxyInitializeEvent event) {
 		// Get Limbo server specified in config
-		this.limboServer = getServerByName(configuration.getProperty("limbo-name"));
+		limboServer = Utility.getServerByName(configurationManager.getProperty("limbo-name"));
 		
 		// Get direct connect fallback server specified in config
-		this.directConnectServer = getServerByName(configuration.getProperty("directconnect-server"));
+		directConnectServer = Utility.getServerByName(configurationManager.getProperty("directconnect-server"));
 		
-		// If either server
-		if(this.limboServer == null || this.directConnectServer == null) {
-			this.server.getEventManager().unregisterListeners(this);
+		// If either server is null, self-destruct
+		if(limboServer == null || directConnectServer == null) {
+			logger.severe("At least one of the specified servers is invalid, VelocityAutoReconnect will not function!");
+			proxyServer.getEventManager().unregisterListeners(this);
 			return;
 		}
 		
+		// Register listeners
+		proxyServer.getEventManager().register(this, new ConnectionListener());
+		proxyServer.getEventManager().register(this, new KickListener());
+		
 		// Schedule the reconnector task
-		server.getScheduler().buildTask(this, () -> {
-			Collection<Player> connectedPlayers = this.limboServer.getPlayersConnected();
+		proxyServer.getScheduler().buildTask(this, () -> {
+			Collection<Player> connectedPlayers = limboServer.getPlayersConnected();
 			// Prevent NullPointerException when Limbo is empty
 			if(connectedPlayers.isEmpty()) return;
-			
 			// "Choose" the next player.
 			Player nextPlayer = connectedPlayers.iterator().next();
-			RegisteredServer previousServer = this.playerData.get(nextPlayer);
+			RegisteredServer previousServer = playerManager.getPreviousServer(nextPlayer);
 			
 			// Redirect the player, if possible.
-			if(previousServer != null) {
-				// If enabled, check if a server responds to pings before connecting
-				if(Boolean.valueOf(this.configuration.getProperty("pingcheck"))) {
+			// If enabled, check if a server responds to pings before connecting
+			try {
+				if(configurationManager.getBooleanProperty("pingcheck")) {
+					// The nested try-catch is probably a horrible solution
 					try {
 						previousServer.ping().join();
-					} catch(CompletionException exception) {
+					} catch(CompletionException completionException) {
+						// Server failed to respond to ping request, return to prevent spam
 						return;
 					}
 				}
-				
-				this.logger.info(String.format("Connecting %s to %s.", nextPlayer.getUsername(), previousServer.getServerInfo().getName()));
-				nextPlayer.createConnectionRequest(previousServer).fireAndForget();
-			} else {
-				this.logger.severe(String.format("Previous server is null for %s .. disconnecting.", nextPlayer.getUsername()));
-				nextPlayer.disconnect(Component.text("Previous server was null.").color(NamedTextColor.YELLOW));
+				logger.info(String.format("Connecting %s to %s.", nextPlayer.getUsername(), previousServer.getServerInfo().getName()));
+				nextPlayer.createConnectionRequest(previousServer).connect();
+			} catch(CompletionException exception) {
+				// Prevent console from being spammed when a server is offline and ping-check is disabled
 			}
 		})
-		.repeat(Integer.valueOf(configuration.getProperty("task-interval-ms")), TimeUnit.MILLISECONDS)
+		.repeat(configurationManager.getIntegerProperty("task-interval-ms"), TimeUnit.MILLISECONDS)
 		.schedule();
 	}
-	
-	@Subscribe(order = PostOrder.FIRST)
-	public void onPlayerPostConnect(ServerPostConnectEvent event) {
-		Player player = event.getPlayer();
-		RegisteredServer previousServer = event.getPreviousServer();
-		
-		if(!player.getCurrentServer().isPresent()) {
-			this.logger.info(String.format("Current server wasn't present for %s.", player.getUsername()));
-			return;
-		}
-		
-		ServerConnection currentServerConnection = player.getCurrentServer().get();
-		
-		/* Set previousServer to directconnect-server if it's not set
-		 * This might happen as a result of direct connection when all other
-		 * servers are offline or the usage of plugins that set the initial server,
-		 * such as RememberMe.
-		 */
-		if(previousServer == null) {
-			if(!this.playerData.containsKey(player)) {
-				previousServer = this.directConnectServer;
-			} else {
-				previousServer = this.playerData.get(player);
-			}
-		}
-		
-		// If a player gets redirected from Limbo to another server, remove them from the Map
-		if(doServerNamesMatch(previousServer, limboServer)) {
-			this.playerData.remove(player);
-			return;
-		}
-		
-		// If a player gets redirected to Limbo from another server, add them to the Map
-		if(doServerNamesMatch(currentServerConnection.getServer(), limboServer)) {
-			this.playerData.put(player, previousServer);
-			this.sendWelcomeMessage(player);
-		}
+
+	public static RegisteredServer getLimboServer() {
+		return limboServer;
+	}
+
+	public static RegisteredServer getDirectConnectServer() {
+		return directConnectServer;
+	}
+
+	public static ProxyServer getProxyServer() {
+		return proxyServer;
+	}
+
+	public static Logger getLogger() {
+		return logger;
+	}
+
+	public static ConfigurationManager getConfigurationManager() {
+		return configurationManager;
+	}
+
+	public static PlayerManager getPlayerManager() {
+		return playerManager;
 	}
 	
-	@Subscribe(order = PostOrder.NORMAL)
-	public void onPlayerKick(KickedFromServerEvent event) {
-		// Check whether the result of the kick actually was a redirection.
-		if(event.getResult() instanceof KickedFromServerEvent.RedirectPlayer) {
-			KickedFromServerEvent.RedirectPlayer playerRedirection = (RedirectPlayer) event.getResult();
-			Player player = event.getPlayer();
-			// Get the kick reason, when possible. Use an empty Component if the kick reason isn't present.
-			Component kickReason = event.getServerKickReason().isPresent() ? event.getServerKickReason().get() : Component.empty();
-			String kickReasonText = kickReason.toString();
-			
-			if(!doServerNamesMatch(playerRedirection.getServer(), limboServer)) {
-				return;
-			}
-			
-			if(Boolean.valueOf(configuration.getProperty("kick-filter.whitelist.enabled")) 
-					&& !this.kickFilterWhitelist.matcher(kickReasonText).matches()) {
-				player.disconnect(kickReason);
-				return;
-			}
-			
-			if(Boolean.valueOf(configuration.getProperty("kick-filter.blacklist.enabled")) 
-					&& this.kickFilterBlacklist.matcher(kickReasonText).matches()) {
-				player.disconnect(kickReason);
-				return;
-			}
-			// Add player and previous server to the Map.
-			this.playerData.put(event.getPlayer(), event.getServer());
-			this.sendWelcomeMessage(player);
-		}
-	}
-	
-	
-	/* Remove anyone who disconnects from the proxy from the playerData Map.
-	 * Note that Map.remove is an optional operation, so we don't need to check whether 
-	 * there actually was a corresponding mapping.
-	 */
-	@Subscribe(order = PostOrder.NORMAL)
-	public void onDisconnect(DisconnectEvent event) {
-		this.playerData.remove(event.getPlayer());
-	}
-	
-	// Returns whether or not the names of the servers match.
-	public boolean doServerNamesMatch(RegisteredServer var0, RegisteredServer var1) {
-		return var0.getServerInfo().getName().equals(var1.getServerInfo().getName());
-	}
-	
-	// Check whether or not the welcome message should be sent and act accordingly
-	public void sendWelcomeMessage(Player player) {
-		if(Boolean.valueOf(configuration.getProperty("message.enabled"))) {
-			player.sendMessage(Component.text(configuration.getProperty("message")));
-		}
-	}
-	
-	public RegisteredServer getServerByName(String serverName) {
-		Optional<RegisteredServer> optionalServer = server.getServer(configuration.getProperty("limbo-name"));
-		
-		if(optionalServer.isPresent()) {
-			return optionalServer.get();
-		}
-		
-		this.logger.severe(String.format("Server \"%s\" is invalid, VelocityAutoReconnect will not function!", serverName));
-		return null;
-	}
 	
 }

src/de/flori4nk/velocityautoreconnect/listeners/ConnectionListener.java
@@ -1,0 +1,63 @@
+package de.flori4nk.velocityautoreconnect.listeners;
+
+import com.velocitypowered.api.event.PostOrder;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.DisconnectEvent;
+import com.velocitypowered.api.event.player.ServerPostConnectEvent;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ServerConnection;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+
+import de.flori4nk.velocityautoreconnect.VelocityAutoReconnect;
+import de.flori4nk.velocityautoreconnect.misc.Utility;
+
+public class ConnectionListener {
+	
+	@Subscribe(order = PostOrder.FIRST)
+	public void onPlayerPostConnect(ServerPostConnectEvent event) {
+		Player player = event.getPlayer();
+		RegisteredServer previousServer = event.getPreviousServer();
+		
+		if(!player.getCurrentServer().isPresent()) {
+			VelocityAutoReconnect.getLogger().info(String.format("Current server wasn't present for %s.", player.getUsername()));
+			return;
+		}
+		
+		ServerConnection currentServerConnection = player.getCurrentServer().get();
+		
+		/* Set previousServer to directconnect-server if it's not set
+		 * This might happen as a result of direct connection when all other
+		 * servers are offline or the usage of plugins that set the initial server,
+		 * such as RememberMe.
+		 */
+		if(previousServer == null) {
+			if(VelocityAutoReconnect.getPlayerManager().isPlayerRegistered(player)) {
+				previousServer = VelocityAutoReconnect.getPlayerManager().getPreviousServer(player);
+			} else {
+				previousServer = VelocityAutoReconnect.getDirectConnectServer();
+			}
+		}
+		
+		// If a player gets redirected from Limbo to another server, remove them from the Map
+		if(Utility.doServerNamesMatch(previousServer, VelocityAutoReconnect.getLimboServer())) {
+			VelocityAutoReconnect.getPlayerManager().removePlayer(player);
+			return;
+		}
+		
+		// If a player gets redirected to Limbo from another server, add them to the Map
+		if(Utility.doServerNamesMatch(currentServerConnection.getServer(), VelocityAutoReconnect.getLimboServer())) {
+			VelocityAutoReconnect.getPlayerManager().addPlayer(player, previousServer);
+			Utility.sendWelcomeMessage(player);
+		}
+	}
+		
+	/* Remove anyone who disconnects from the proxy from the playerData Map.
+	 * Note that Map.remove is an optional operation, so we don't need to check whether 
+	 * there actually was a corresponding mapping.
+	 */
+	@Subscribe(order = PostOrder.NORMAL)
+	public void onDisconnect(DisconnectEvent event) {
+		VelocityAutoReconnect.getPlayerManager().removePlayer(event.getPlayer());
+	}
+	
+}

src/de/flori4nk/velocityautoreconnect/listeners/KickListener.java
@@ -1,0 +1,55 @@
+package de.flori4nk.velocityautoreconnect.listeners;
+
+import java.util.regex.Pattern;
+
+import com.velocitypowered.api.event.PostOrder;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.player.KickedFromServerEvent;
+import com.velocitypowered.api.event.player.KickedFromServerEvent.RedirectPlayer;
+import com.velocitypowered.api.proxy.Player;
+import de.flori4nk.velocityautoreconnect.VelocityAutoReconnect;
+import de.flori4nk.velocityautoreconnect.misc.Utility;
+import net.kyori.adventure.text.Component;
+
+public class KickListener {
+
+	private Pattern kickFilterWhitelist;
+	private Pattern kickFilterBlacklist;
+	
+	public KickListener() {
+		// Compile whitelist / blacklist Patterns from configured expressions
+		this.kickFilterBlacklist = Pattern.compile(VelocityAutoReconnect.getConfigurationManager().getProperty("kick-filter.blacklist"), Pattern.DOTALL);
+		this.kickFilterWhitelist = Pattern.compile(VelocityAutoReconnect.getConfigurationManager().getProperty("kick-filter.whitelist"), Pattern.DOTALL);
+	}
+	
+	@Subscribe(order = PostOrder.NORMAL)
+	public void onPlayerKick(KickedFromServerEvent event) {
+		// Check whether the result of the kick actually was a redirection.
+		if(event.getResult() instanceof KickedFromServerEvent.RedirectPlayer) {
+			KickedFromServerEvent.RedirectPlayer playerRedirection = (RedirectPlayer) event.getResult();
+			Player player = event.getPlayer();
+			// Get the kick reason, when possible. Use an empty Component if the kick reason isn't present.
+			Component kickReason = event.getServerKickReason().isPresent() ? event.getServerKickReason().get() : Component.empty();
+			String kickReasonText = kickReason.toString();
+			
+			if(!Utility.doServerNamesMatch(playerRedirection.getServer(), VelocityAutoReconnect.getLimboServer())) {
+				return;
+			}
+			
+			if(VelocityAutoReconnect.getConfigurationManager().getBooleanProperty("kick-filter.whitelist.enabled") 
+					&& !this.kickFilterWhitelist.matcher(kickReasonText).matches()) {
+				player.disconnect(kickReason);
+				return;
+			}
+			
+			if(VelocityAutoReconnect.getConfigurationManager().getBooleanProperty("kick-filter.blacklist.enabled")
+					&& this.kickFilterBlacklist.matcher(kickReasonText).matches()) {
+				player.disconnect(kickReason);
+				return;
+			}
+			// Add player and previous server to the Map.
+			VelocityAutoReconnect.getPlayerManager().addPlayer(event.getPlayer(), event.getServer());
+			Utility.sendWelcomeMessage(player);
+		}
+	}
+}

src/de/flori4nk/velocityautoreconnect/storage/ConfigurationManager.java
@@ -1,0 +1,57 @@
+package de.flori4nk.velocityautoreconnect.storage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationManager {
+	
+	private Properties properties;
+	
+	public ConfigurationManager(File configurationFile) {
+		this.properties = new Properties();
+		
+		try {
+			// Create the configuration file if it doesn't exist.
+			if(!configurationFile.exists()) {
+				configurationFile.getParentFile().mkdirs();
+				configurationFile.createNewFile();
+			}
+			
+			// Set default values
+			this.properties.setProperty("limbo-name", "limbo");
+			this.properties.setProperty("directconnect-server", "lobby");
+			this.properties.setProperty("task-interval-ms", "3500");
+			this.properties.setProperty("kick-filter.blacklist", ".* ([Bb]anned|[Kk]icked).*");
+			this.properties.setProperty("kick-filter.blacklist.enabled", "false");
+			this.properties.setProperty("kick-filter.whitelist", "Server closed");
+			this.properties.setProperty("kick-filter.whitelist.enabled", "true");
+			this.properties.setProperty("message", "You will be reconnected soon.");
+			this.properties.setProperty("message.enabled", "false");
+			this.properties.setProperty("pingcheck", "true");
+			
+			// Load saved values
+			this.properties.load(new FileInputStream(configurationFile));
+			
+			// Save old values combined with possible new defaults
+			this.properties.store(new FileOutputStream(configurationFile), "VelocityAutoReconnect configuration -- Documentation: https://github.com/flori4nk/VelocityAutoReconnect/blob/master/README.md#configuration");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getProperty(String key) {
+		return this.properties.getProperty(key, "");
+	}
+	
+	public boolean getBooleanProperty(String key) {
+		return Boolean.valueOf(this.getProperty(key));
+	}
+	
+	public int getIntegerProperty(String key) {
+		return Integer.valueOf(this.getProperty(key));
+	}
+	
+}

src/de/flori4nk/velocityautoreconnect/storage/PlayerManager.java
@@ -1,0 +1,35 @@
+package de.flori4nk.velocityautoreconnect.storage;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+
+import de.flori4nk.velocityautoreconnect.VelocityAutoReconnect;
+
+public class PlayerManager {
+	
+	private Map<Player, RegisteredServer> playerData;
+	
+	public PlayerManager() {
+		this.playerData = new LinkedHashMap<>();
+	}
+	
+	public void addPlayer(Player player, RegisteredServer registeredServer) {
+		this.playerData.put(player, registeredServer);
+	}
+	
+	public void removePlayer(Player player) {
+		this.playerData.remove(player);
+	}
+	
+	public RegisteredServer getPreviousServer(Player player) {
+		return this.playerData.getOrDefault(player, VelocityAutoReconnect.getDirectConnectServer());
+	}
+	
+	public boolean isPlayerRegistered(Player player) {
+		return playerData.containsKey(player);
+	}
+
+}

commit ecccecf46ad2acd66e3a3f000ae34e74cb0053d2

Fix copy-paste error in Utility#getServerByName
Copy-pasted code from VelocityAutoReconnect.java to define the method in Utility.java, made a grave mistake thanks to lack of caffeine and forgot to replace the hardcoded serverName as well as the call in VelocityAutoReconnect.java. Commit 8205a3c7c835269e00c39f7ce04fcd64ecb04f01 broke getting servers.

src/de/flori4nk/velocityautoreconnect/misc/Utility.java
@@ -39,7 +39,7 @@ public class Utility {
 	}
 		
 	public static RegisteredServer getServerByName(String serverName) {
-		Optional<RegisteredServer> optionalServer = VelocityAutoReconnect.getProxyServer().getServer(VelocityAutoReconnect.getConfigurationManager().getProperty("limbo-name"));
+		Optional<RegisteredServer> optionalServer = VelocityAutoReconnect.getProxyServer().getServer(serverName);
 			
 		if(optionalServer.isPresent()) {
 			return optionalServer.get();
@@ -47,6 +47,10 @@ public class Utility {
 			
 		VelocityAutoReconnect.getLogger().severe(String.format("Server \"%s\" is invalid, VelocityAutoReconnect will not function!", serverName));
 		return null;
+	}
+
+	public static RegisteredServer getServerFromProperty(String propertyName) {
+		return getServerByName(VelocityAutoReconnect.getConfigurationManager().getProperty(propertyName));
 	}
 	
 	public static void logInformational(String message) {